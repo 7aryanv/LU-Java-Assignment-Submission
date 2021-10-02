bcda7459e405ee9e883a6e9190d98335668c2ae7
Aryan Vyas <91691943+7aryanv@users.noreply.github.com>
2021-10-02 16:32:52 +0530

    Assignment 1

---
+++ Assignment1.java
@@ -0,0 +1,10 @@
+package com.aryan.vyas;
+
+// Program for Printing My Information
+public class Assignment1 {
+    public static void main(String[] args) {
+        System.out.println("Name : Aryan Vyas"); //Printing Name
+        System.out.println("Age : 21 Years"); //Printing Age
+        System.out.println("State : Gujarat"); //Printing State
+    }
+}
